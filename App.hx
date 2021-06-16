3b0115c33becd9f8ff63dd1c62fb691cc9fd2b2e
tong <tong@disktree.net>
2021-06-16 11:55:29 +0200

    Add export svg, css, gpl functionality

--- App.hx
+++ App.hx
@@ -1,10 +1,15 @@
 
 import hsluv.Hsluv;
-import js.Browser.document;
-import js.Browser.window;
-import js.html.svg.SVGElement;
-import js.html.DOMParser;
+import js.Syntax;
+import om.Browser;
+import om.Browser.document;
+import om.Browser.window;
+import om.Random;
 
+var colors : Array<Array<String>>;
+var svgStr : String;
+
+@:expose
 function generate() {
 
 	//var invertInv = true;
@@ -25,7 +30,7 @@ function generate() {
 	var darkL = getRandomInt( 10, 50 );
 	var rangeL = 100 - darkL;
 
-	trace(backS,darkL,rangeL);
+	//trace(backS,darkL,rangeL);
 
 	fg[0] = Hsluv.hsluvToHex( [H[ 4 ], 70, darkL + rangeL * Math.pow(3 / 6, 1.5)] );
 	fg[1] = Hsluv.hsluvToHex( [H[ 3 ], 50, darkL + rangeL * Math.pow(2 / 6, 1.5)] );
@@ -34,7 +39,7 @@ function generate() {
 	fg[3] = bg[2];
 	bg[4] = fg[0];
 
-	trace(bg,fg);
+	colors = [bg,fg];
 
 	/*
 	var minS = getRandomInt( 30, 70 );
@@ -51,22 +56,7 @@ function generate() {
 
 	document.body.style.background = bg[0];
 
-	/* var svg = new DOMParser().parseFromString('<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-	<rect width="96" height="64" id="background" fill="${bg[0]}"></rect>
-	<!-- Foreground -->
-	<circle cx="24" cy="24" r="8" id="f_high" fill="${fg[0]}"></circle>
-	<circle cx="40" cy="24" r="8" id="f_med" fill="${fg[1]}"></circle>
-	<circle cx="56" cy="24" r="8" id="f_low" fill="${fg[2]}"></circle>
-	<circle cx="72" cy="24" r="8" id="f_inv" fill="${fg[3]}"></circle>
-	<!-- Background -->
-	<circle cx="24" cy="40" r="8" id="b_high" fill="${bg[1]}"></circle>
-	<circle cx="40" cy="40" r="8" id="b_med" fill="${bg[2]}"></circle>
-	<circle cx="56" cy="40" r="8" id="b_low" fill="${bg[3]}"></circle>
-	<circle cx="72" cy="40" r="8" id="b_inv" fill="${bg[4]}"></circle>
-  </svg>', IMAGE_SVG_XML ).getElementsByTagNameNS("http://www.w3.org/2000/svg", "svg").item(0); */
-	// document.body.append( svg );
-
-	document.body.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100%" viewBox="0 0 96 48">
+	svgStr = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100%" viewBox="0 0 96 64">
 	<rect width="96" height="64" id="background" fill="${bg[0]}"></rect>
 	<!-- Foreground -->
 	<circle cx="24" cy="24" r="8" id="f_high" fill="${fg[0]}"></circle>
@@ -79,23 +69,77 @@ function generate() {
 	<circle cx="56" cy="40" r="8" id="b_low" fill="${bg[3]}"></circle>
 	<circle cx="72" cy="40" r="8" id="b_inv" fill="${bg[4]}"></circle>
   </svg>';
+
+	document.body.querySelector('main').innerHTML = svgStr;
+}
+
+@:expose
+function exportSVG( name = "theme.svg" ) {
+	Browser.saveTextFile( name, svgStr );
+}
+
+@:expose
+function exportGPL( name = "theme.gpl" ) {
+	var gplColors = new Array<om.color.GimpPalette.Color>();
+	function add( name : String, color : String ) {
+		var rgb = hexToRgb( color );
+		gplColors.push({ name : name, r: rgb[0], g: rgb[1], b: rgb[2] });
+	}
+	add( 'background', colors[0][0] );
+	add( 'f_high', colors[1][0] );
+	add( 'f_med', colors[1][1] );
+	add( 'f_low', colors[1][2] );
+	add( 'f_inv', colors[1][3] );
+	add( 'b_high', colors[0][1] );
+	add( 'b_med', colors[0][2] );
+	add( 'b_low', colors[0][3] );
+	add( 'b_inv', colors[0][4] );
+	var gpl = new om.color.GimpPalette( 'husl', 1,gplColors );
+	Browser.saveTextFile( name, gpl.toString() );
+}
+
+@:expose
+function exportCSS( name = "theme.css" ) {
+	var css = ':root {\n';
+	css += '\t--background: ${colors[0][0]};\n';
+	css += '\t--f_high: ${colors[1][0]};\n';
+	css += '\t--f_med: ${colors[1][1]};\n';
+	css += '\t--f_low: ${colors[1][2]};\n';
+	css += '\t--f_inv: ${colors[1][3]};\n';
+	css += '\t--b_high: ${colors[0][1]};\n';
+	css += '\t--b_med: ${colors[0][2]};\n';
+	css += '\t--b_low: ${colors[0][3]};\n';
+	css += '\t--b_inv: ${colors[0][4]};\n';
+	css += '}\n';
+	Browser.saveTextFile( name, css );
 }
 
 inline function getRandomInt( min : Int, max : Int )
-	return om.Random.int( max - min ) + min;
+	return Random.int( max - min ) + min;
+
+function hexToRgb( hex : String ) : Array<Int> {
+	var ereg = ~/^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i;
+	if( !ereg.match( hex ) )
+		return null;
+	return [
+		Syntax.code( "parseInt({0},16)", ereg.matched(1) ),
+		Syntax.code( "parseInt({0},16)", ereg.matched(2) ),
+		Syntax.code( "parseInt({0},16)", ereg.matched(3) ),
+	];
+}
 
 function main() {
-	window.onkeydown = generate;
-	/*e -> switch e.key {
-	case "r": generate();
-	case _:
+	window.onkeydown = e -> switch e.key {
+	case _:  generate();
 	}
-	*/
-	document.onclick = generate;
-	document.onwheel = generate;
+	document.body.querySelector('main').onclick = e -> generate();
+	//document.onwheel = generate;
 	/* document.oncontextmenu =e -> {
 		e.preventDefault();
 		generate();
 	} */
 	generate();
+	document.body.querySelector('header>button[name="gpl"]').onclick = e -> exportGPL();
+	document.body.querySelector('header>button[name="svg"]').onclick = e -> exportSVG();
+	document.body.querySelector('header>button[name="css"]').onclick = e -> exportCSS();
 }
